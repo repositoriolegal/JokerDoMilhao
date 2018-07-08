680c20303a4c9b39506a88e299a1b9fd7a9e45be
Tulio Cesar <tulio@intelivix.com>
2018-07-08 19:26:25 -0300

    Nova alternativa para acessar os dados das partidas.

--- Sofa/sofascore.py
+++ Sofa/sofascore.py
@@ -2,6 +2,24 @@ import requests
 import csv
 from objectpath import Tree
 
+URL_MATCH = 'https://www.sofascore.com/pt/{slug_match}/{match_custom_id}'
+URL_MATCHES = "https://www.sofascore.com/tournament/24/{id_tournament}/standings/tables/json"
+
+ALLSVENSKAN_ID = 15731
+
+def get_id_matches():
+    response = requests.get(URL_MATCHES.format(id_tournament=ALLSVENSKAN_ID))
+
+    # Retornar lista de URLs de cada partida
+    # URL.format(slug_match=,match_id)
+    # slug_match e match_id estão contidos no json da response acima
+    # Como acessar o Response de retorno: response.json()['teamEvents']['1768']['away'][0]
+    # Opções de cada dicionário:
+    # Dic_1: 'standingsTables', 'teamEvents'
+    # Dic_2: '1768', '1762', '1759', '1769', '1761', '1891', '1771',
+    # '1764', '1760', '1763', '22260', '1787', '1793', '1892', '1758', '1836'
+    # Dic_3: 'total', 'home', 'away'
+    return 'hello'
 
 def search_result(query):
     SEARCH_URL = 'https://www.sofascore.com/search/results?q={}'.format
@@ -38,9 +56,11 @@ def get_team_ids(tournament_id):
 
 
 if __name__ == "__main__":
-    team_info = search_result('Allsvenskan')
+    get_id_matches()
+ 
+    """team_info = search_result('Allsvenskan')
     with open('output.csv', 'w+') as f:
         w = csv.DictWriter(f, ['name', 'id', 'slug', 'url'])
         w.writeheader()
         for item in team_info:
-            w.writerow(item)
+            w.writerow(item)"""
