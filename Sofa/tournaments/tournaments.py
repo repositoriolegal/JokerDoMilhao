718f8b922c1a02cd39b1273fd0a554fcfd249982
Tulio Cesar <tulio.silva@br.experian.com>
2018-12-28 10:42:27 -0200

    Add new class Allsvenskan, responsible for Allsvenskan flow.

---
+++ Sofa/tournaments/tournaments.py
@@ -0,0 +1,6 @@
+from constants import ALLSVENSKAN_ID, ALLSVENSKAN_SEASON2018_ID
+from tournaments.base import BaseTournament
+
+class Allsvenskan(BaseTournament):
+    TOURNAMENT_ID = ALLSVENSKAN_ID
+    TOURNAMENT_SEASON_ID = ALLSVENSKAN_SEASON2018_ID
