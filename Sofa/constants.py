94d035985fbb71262e5c5512ff7bf81a1af4650c
Tulio Cesar <tulio.silva@br.experian.com>
2018-12-28 10:35:47 -0200

    Add new file with Tournaments and Allsvenskan constants.

---
+++ Sofa/constants.py
@@ -0,0 +1,9 @@
+# Tournament Constants
+URL_TOURNAMENT = "https://www.sofascore.com/u-tournament/{id_tournament}/season/{id_season}/json?_="
+URL_ROUND_MATCHES = "https://www.sofascore.com/u-tournament/{id_tournament}/season/{id_season}/matches/round/{round_number}?_="
+URL_MATCH_DATA = "https://www.sofascore.com/event/{match_id}/json?_="
+
+# Allsvenskan Constants
+ALLSVENSKAN_ID = 40
+ALLSVENSKAN_SEASON2018_ID = 15731
+ALLSVENSKAN_SEASON2019_ID = 19949
