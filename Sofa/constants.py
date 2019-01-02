d2b4ac7f469a1602511b5adeafce4b764b102b59
Tulio Cesar <tulio.silva@br.experian.com>
2019-01-02 13:30:44 -0200

    Add MLS tournament constants.

--- Sofa/constants.py
+++ Sofa/constants.py
@@ -1,9 +1,14 @@
 # Tournament Constants
 URL_TOURNAMENT = "https://www.sofascore.com/u-tournament/{id_tournament}/season/{id_season}/json?_="
 URL_ROUND_MATCHES = "https://www.sofascore.com/u-tournament/{id_tournament}/season/{id_season}/matches/round/{round_number}?_="
+URL_WEEK_MATCHES = "https://www.sofascore.com/u-tournament/{id_tournament}/season/{id_season}/matches/week/{start_date}/{end_date}"
 URL_MATCH_DATA = "https://www.sofascore.com/event/{match_id}/json?_="
 
 # Allsvenskan Constants
 ALLSVENSKAN_ID = 40
 ALLSVENSKAN_SEASON2018_ID = 15731
 ALLSVENSKAN_SEASON2019_ID = 19949
+
+# Mls Constants
+MLS_ID = 242
+MLS_SEASON2018_ID = 15813
