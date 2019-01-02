3090d7b27d8c1db53f6374059f1752a63854afe2
Tulio Cesar <tulio.silva@br.experian.com>
2019-01-02 13:30:04 -0200

    Add MLS tournament class.

--- Sofa/tournaments/tournaments.py
+++ Sofa/tournaments/tournaments.py
@@ -1,6 +1,13 @@
-from constants import ALLSVENSKAN_ID, ALLSVENSKAN_SEASON2018_ID
+from constants import ALLSVENSKAN_ID, ALLSVENSKAN_SEASON2018_ID, MLS_ID, MLS_SEASON2018_ID
 from tournaments.base import BaseTournament
 
 class Allsvenskan(BaseTournament):
     TOURNAMENT_ID = ALLSVENSKAN_ID
     TOURNAMENT_SEASON_ID = ALLSVENSKAN_SEASON2018_ID
+    BY_ROUND = True
+
+
+class MLS(BaseTournament):
+    TOURNAMENT_ID = MLS_ID
+    TOURNAMENT_SEASON_ID = MLS_SEASON2018_ID
+    BY_MATCH = True
