8a6770df50363881dc8ae7f5e5e1ee56cd7f5f95
tuliosilva <tulio.silva@hbsis.com.br>
2019-07-03 10:10:25 -0300

    Send information to Elasticsearch

--- Sofa/sofascore.py
+++ Sofa/sofascore.py
@@ -1,9 +1,25 @@
-from stats import Stats
+import logging
+import sys
+
+from repositories import ElasticSearch
 from tournaments import Allsvenskan, BRSerieA, Allsvenskan, MLS, BRSerieA, BRSerieB, PremierLeagueEngland
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s - %(message)s'
+)
 
 
 if __name__ == "__main__":
     allsvenskan = Allsvenskan()
+
     events = allsvenskan.get_matches_events()
-    stats = Stats(events)
-    stats.tournament_goals_stats().to_csv('./goals_stats.csv')
+
+    logging.info('Start to send to Elasticsearch')
+
+    es = ElasticSearch()
+    for event in events:
+        es.insert(event)
+
+    logging.info('Finish to send information to Elasticsearch')
